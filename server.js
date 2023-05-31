8732f3339555c06e69b25608e33dc105e1d4f6a8
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-31 23:53:33 +0800

    installed nodemon and other stuff
    added the db connection but need to import express

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("hello word");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
+const PORT = process.env.PORT || 3500;
+
+mongoose.connection.once("open", () => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
