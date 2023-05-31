286d7065142305ec93353d362b87169082ee9a7f
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-31 23:56:12 +0800

    installed cors and express

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
 const connectDB = require("./config/dbConn");
 const PORT = process.env.PORT || 3500;
+
+// Connect to MongoDB
+connectDB();
+
+// Cross Origin Resource Sharing
+app.use(cors());
 
 mongoose.connection.once("open", () => {
     console.log("Connected to MongoDB");
