e4782b0fd8200a9e754c05764648e95e04647498
Frederick Pua <puachengxuan.fp@gmail.com>
2023-06-01 14:25:03 +0800

    added more allowedorigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
     "https://www.frederickpua.com",
+    "https://www.frederickpua.com/",
+    "https://frederickpua.com/",
     "https://frederickpua.com",
     "http://127.0.0.1:5500",
     "http://localhost:3500",
