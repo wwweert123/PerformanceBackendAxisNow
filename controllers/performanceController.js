c5cec947b7037075767834b8928912b76a06f5ac
Frederick Pua <puachengxuan.fp@gmail.com>
2023-06-12 00:16:20 +0800

    Update performanceController.js

--- controllers/performanceController.js
+++ controllers/performanceController.js
@@ -23,22 +23,6 @@ const createVisit = async (req, res) => {
     } catch (err) {
         res.status(400).json(err);
     }
-    // if (!req?.body?.firstname || !req?.body?.lastname) {
-    //     return res
-    //         .status(400)
-    //         .json({ message: "First and last names are required" });
-    // }
-
-    // try {
-    //     const result = await Employee.create({
-    //         firstname: req.body.firstname,
-    //         lastname: req.body.lastname,
-    //     });
-
-    //     res.status(201).json(result);
-    // } catch (err) {
-    //     console.error(err);
-    // }
 };
 
 module.exports = { createVisit };
